4996fc5804f8a4bcd0aaa5d04b247878df036c4e
Yu Jia <yujiatay@gmail.com>
2018-06-07 22:26:39 +0800

    Webapp: Configured firebase

---
+++ webapp/src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyAe-kdYNRZJkY90rspVXhmCNqlbhAAncqg",
+  authDomain: "locatemd.firebaseapp.com",
+  databaseURL: "https://locatemd.firebaseio.com",
+  projectId: "locatemd",
+  storageBucket: "locatemd.appspot.com",
+  messagingSenderId: "385392707364"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+  auth,
+};
