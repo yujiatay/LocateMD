3135d3090a9ee0e2e218f395e9818120910f74bc
Unknown <phuajohn0@gmail.com>
2018-06-08 01:23:47 +0800

    fix: Temp fix to prevent errors from null authUser

--- webapp/src/components/Account.jsx
+++ webapp/src/components/Account.jsx
@@ -8,7 +8,7 @@ const AccountPage = () => (
   <AuthUserContext.Consumer>
     {authUser => (
       <div>
-        <h1>Account: {authUser.email}</h1>
+        <h1>Account: {authUser? authUser.email: ""}</h1>
         <PasswordForgetForm />
         <PasswordChangeForm />
       </div>
