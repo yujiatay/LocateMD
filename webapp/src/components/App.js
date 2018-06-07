3048aacb03a02d3f88405a688b7cec0e07a25669
Yu Jia <yujiatay@gmail.com>
2018-06-07 23:16:19 +0800

    Webapp: Integrated firebase authentication

--- webapp/src/components/App.js
+++ webapp/src/components/App.js
@@ -13,6 +13,7 @@ import HomePage from './Home';
 import AccountPage from './Account';
 
 import * as routes from '../constants/routes';
+import withAuthentication from './withAuthentication';
 
 const App = () =>
   <Router>
@@ -21,31 +22,13 @@ const App = () =>
 
       <hr/>
 
-      <Route
-        exact path={routes.LANDING}
-        component={() => <LandingPage />}
-      />
-      <Route
-        exact path={routes.SIGN_UP}
-        component={() => <SignUpPage />}
-      />
-      <Route
-        exact path={routes.SIGN_IN}
-        component={() => <SignInPage />}
-      />
-      <Route
-        exact path={routes.PASSWORD_FORGET}
-        component={() => <PasswordForgetPage />}
-      />
-      <Route
-        exact path={routes.HOME}
-        component={() => <HomePage />}
-      />
-      <Route
-        exact path={routes.ACCOUNT}
-        component={() => <AccountPage />}
-      />
+      <Route exact path={routes.LANDING} component={() => <LandingPage />} />
+      <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
+      <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
+      <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
+      <Route exact path={routes.HOME} component={() => <HomePage />} />
+      <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
     </div>
   </Router>
 
-export default App;
+export default withAuthentication(App);
