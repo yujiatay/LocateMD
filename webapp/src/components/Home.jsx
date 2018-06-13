eec9f44ccb9dca76b9a164465b040e0def1a2b07
Yu Jia <yujiatay@gmail.com>
2018-06-13 22:04:03 +0800

    Webapp: Added home page (WIP)

--- webapp/src/components/Home.jsx
+++ webapp/src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
+import { Layout, Card, Row, Col, Icon} from 'antd';
 
 import withAuthorization from './withAuthorization';
 import { database } from '../firebase';
+import './Home.css';
+
+const { Header, Content } = Layout;
 
 class HomePage extends Component {
   constructor(props) {
@@ -23,16 +27,37 @@ class HomePage extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Home</h1>
-        <p>The Home Page is accessible by every signed in user.</p>
+      <div className="home">
+        <Layout>
+          <Header>
+            <p>Welcome { this.props.authUser.email }</p>
+          </Header>
+          <Content>
+          <Row gutter={16}>
+            <Col span={8}>
+              <Card bordered={false}>
+                <Icon type="smile-o" />
+                <p className="big-number">40</p>
+                <p>patients in queue</p>
+              </Card>
+            </Col>
+            <Col span={8}>
+              <Card bordered={false}>
+                <Icon type="smile-o" />
+                <p className="big-number">100</p>
+                <p>patients served today</p>
+              </Card>
+            </Col>
+          </Row>
+          </Content>
+        </Layout>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  users: state.userState.users
+  authUser: state.sessionState.authUser
 });
 
 const mapDispatchToProps = dispatch => ({
