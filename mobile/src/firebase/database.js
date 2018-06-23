dfb03f76cff7c7aa2919ac55ed061d0c48f2d1a8
John Phua <phuajohn0@gmail.com>
2018-06-23 22:40:52 +0800

    Mobile: Added book appointment and join queue functions

--- mobile/src/firebase/database.js
+++ mobile/src/firebase/database.js
@@ -1,8 +1,9 @@
 import { database, auth } from './firebase';
 
-export const updateInfo = (id, email, contactNumber = '') =>
-  database.ref(`clinics/${id}`).set(
+export const updateInfo = (id, email, contactNumber = '') => {
+  database.ref(`patients/${id}`).set(
     {
+      // DEETS TBC
       contactNumber: contactNumber,
       email: email
     },
@@ -14,3 +15,43 @@ export const updateInfo = (id, email, contactNumber = '') =>
       }
     }
   );
+};
+
+export const getInfo = id => {
+
+  if (id === undefined) {
+    id = auth.currentUser.uid;
+  }
+
+  database
+  .ref(`patients/${id}`)
+  .once('value')
+  .then(function (snapshot) {
+    return snapshot.val();
+  });
+};
+
+export const bookAppointment = (timestamp, clinicID) => {
+  let patientID = auth.currentUser.uid;
+  let appointmentRef = database.ref('appointments').push();
+  let newAppointment = {
+    clinic: clinicID,
+    patient: patientID,
+    startTime: timestamp,
+    isBooking: true
+  };
+  appointmentRef.set(newAppointment);
+};
+
+export const joinQueue = (clinicID) => {
+  let patientID = auth.currentUser.uid;
+  let appointmentRef = database.ref('appointments').push();
+  let newAppointment = {
+    clinic: clinicID,
+    patient: patientID,
+    // placeholder
+    startTime: Date.now(),
+    isBooking: false
+  };
+  clinicApptRef.set(newAppointment);
+};
