fad6c1928ef59e4a81da59a666cce735a0dbf7b2
Yu Jia <yujiatay@gmail.com>
2018-06-25 18:15:46 +0800

    Mobile: Added Roboto Light

--- mobile/App.js
+++ mobile/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
     await Expo.Font.loadAsync({
       'Roboto': require('native-base/Fonts/Roboto.ttf'),
       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      'Roboto_light': require('./src/assets/Roboto-Light.ttf'),
     });
     this.setState({ isReady: true });
   }
