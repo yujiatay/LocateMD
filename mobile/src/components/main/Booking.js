4c7ad5fc1802deb0645fb2a7c8889cb71b5c5723
Yu Jia <yujiatay@gmail.com>
2018-06-30 00:52:14 +0800

    Mobile: Added success and failure dialog, resolves #20

--- mobile/src/components/main/Booking.js
+++ mobile/src/components/main/Booking.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, TouchableOpacity, Image } from 'react-native';
 import { Card, CardItem, Text, Body } from 'native-base';
 import { database } from '../../firebase';
 import * as datelib from '../../datelib';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 
 const DetailItem = ({name, item}) => (
   <View>
@@ -65,28 +66,82 @@ class BookingSlots extends React.Component {
     );
   }
 }
-import clinicPlaceHolderImage from '../../assets/clinic_placeholder.jpg'
+import clinicPlaceHolderImage from '../../assets/clinic_placeholder.jpg';
+import PopupDialog, { ScaleAnimation, DialogButton } from 'react-native-popup-dialog';
+import { ActivityIndicator, Modal } from 'react-native';
+
+class SpinnerOverlay extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    const loading = this.props.loading;
+    return (
+      <Modal transparent={true} animationType={'none'} visible={loading} onRequestClose={()=>{alert('modal closed')}}>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'space-around',
+          backgroundColor: '#00000040'}}>
+          <View style={{backgroundColor: '#fff', height: 100, width: 100,
+          borderRadius: 10, display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
+            <ActivityIndicator size="large" animating={loading}/>
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+}
+
 class Booking extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       timeslots: datelib.filterDaySlots((datelib.genDaySlots()), this.props.navigation.state.params.clinic.bookedSlots),
-      chosenSlot: -1
+      chosenSlot: -1,
+      loading: false,
+      bookingResult: ''
     };
     this.bookingSlotHandler = this.bookingSlotHandler.bind(this);
   }
-
   bookingSlotHandler(chosenSlot) {
     this.setState({
       chosenSlot: parseInt(chosenSlot) - 1
     });
   }
-
+  _bookAppointment = async () => {
+    this.setState({ loading : true });
+    let response = await database.bookAppointment(this.state.timeslots[this.state.chosenSlot],
+      this.props.navigation.state.params.clinic.clinicID);
+    setTimeout(() => {
+      this.setState({ loading: false, bookingResult: response === null ? false : true });
+      this.popupDialog.show();
+    }, 2500);
+  };
   render() {
+    const scaleAnimation = new ScaleAnimation();
     const clinic = this.props.navigation.state.params.clinic;
     const address = clinic.address.blockNo + " " + clinic.address.streetName + " S" + clinic.address.postalCode;
     return (
       <View style={styles.container}>
+        <SpinnerOverlay loading={this.state.loading}/>
+        <PopupDialog
+          ref={(popupDialog) => { this.popupDialog = popupDialog; }}
+          dialogAnimation={scaleAnimation}
+          dialogStyle={{elevation: 1, width: '80%', height: '30%' }}
+          containerStyle={{elevation: 10}}
+          actions={[
+            <DialogButton text="DONE"
+              textStyle={{fontFamily: 'Roboto_light'}}
+              onPress={() => {this.popupDialog.dismiss();}}
+              key="done-button"
+            />,
+          ]}
+        >
+          <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+            { this.state.bookingResult
+              ? <SimpleLineIcons name="check" size={100} color="#00cd00"/>
+              : <SimpleLineIcons name="close" size={100} color="#e50000"/>}
+            <Text style={{fontFamily: 'Roboto_light'}}>{this.state.bookingResult ? "SUCCESS" : "FAIL"}</Text>
+          </View>
+        </PopupDialog>
         <View style={{flex: 2}}>
           <Image source={clinicPlaceHolderImage} style={{ flex: 1, width: '100%', height: null, resizeMode: 'cover' }}/>
         </View>
@@ -106,8 +161,7 @@ class Booking extends React.Component {
           </Card>
           <Card style={styles.card}>
             <CardItem button
-                      onPress={() => database.bookAppointment(this.state.timeslots[this.state.chosenSlot],
-                        clinic.clinicID)}
+                      onPress={this._bookAppointment}
                       style={styles.cardItem}>
               <Body style={styles.cardItem}>
               <Text style={styles.cardText}>Book in advance</Text>
