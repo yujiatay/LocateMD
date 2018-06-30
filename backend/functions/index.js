6e659cd3d45024ebee3c35b339a923f8f8eabaea
John Phua <phuajohn0@gmail.com>
2018-07-01 02:37:34 +0800

    Backend: Add name and queue number to appointments, remove deprecated

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -13,118 +13,21 @@ exports.updateClinicStamp = functions.database.ref("clinics").onWrite((change, c
   return admin.database().ref("clinicMeta").update({lastModified: Date.now()});
 });
 
-// // TODO: Check if really necessary (alot cheaper without)
-// exports.syncAppointment = functions.database.ref("appointmentsclinic/{uid}/{entry}").onWrite((change, context) => {
-//   let snapshot = {};
-//   let patientUID = "";
-//   console.log(context);
-//   if (!change.before.exists()) {
-//     // Create operation
-//     snapshot = change.after.val();
-//     patientUID = change.after.val().patient;
-//   } else if (!change.after.exists()) {
-//     // Delete operation
-//     snapshot = change.after.val();
-//     patientUID = change.before.val().patient;
-//   } else {
-//     let beforeUID = change.before.val().patient;
-//     let afterUID = change.after.val().patient;
-//
-//     // Update operation (same patientUID)
-//     if (beforeUID === afterUID) {
-//       patientUID = afterUID;
-//       snapshot = change.after.val();
-//     } else {
-//       patientUID = afterUID;
-//       snapshot = change.after.val();
-//       admin.database().ref("appointmentspatient/" + context.params.uid + "/" + context.params.entry).update(null);
-//     }
-//   }
-//   console.log("DUPE DATA");
-//   console.log(snapshot);
-//   console.log(context.params.entry);
-//   if (snapshot === null) {
-//     return admin.database().ref("appointmentspatient/" + patientUID + "/" + context.params.entry).remove();
-//   } else {
-//     return admin.database().ref("appointmentspatient/" + patientUID + "/" + context.params.entry).update(snapshot);
-//   }
-// });
-//
-// // Function for joining queue, to determine the next queue time, correlate with advance booking slots with buffer
-// exports.updateRealTimeEstimatesOnClinicEntry = functions.database.ref("appointmentsclinic/{uid}/{entry}").onCreate((snapshot, context) => {
-//   const appt = snapshot.val();
-//   let clinicRef = admin.database().ref("clinics/" + appt.clinic);
-//   // TODO: possible cleanup on old booking slots
-//   if (!appt.isBooking) {
-//     return clinicRef.transaction((clinic) => {
-//       if (clinic) {
-//         // Obtain current slot times
-//         let startTime = clinic.nextEstimate;
-//         if (!clinic.hasOwnProperty('bookedSlots') || clinic.estimatedServiceTime === null) clinic.estimatedServiceTime = 600000;
-//         let endTime = startTime + clinic.estimatedServiceTime;
-//
-//         // In form of a json object, key: starttime, value: endtime
-//         let bookingList = (!clinic.hasOwnProperty('bookedSlots') || clinic.bookedSlots === null) ? {} : clinic.bookedSlots;
-//         // Likely to cause problems if estimate is >3 hours (conflict with booking slots)
-//         // Check if estimatedStartTime is accurate
-//         if (!clinic.hasOwnProperty('nextEstimate') || clinic.nextEstimate === null ||
-//           clinic.nextEstimate < (Date.now() - 60000)) {
-//           // estimate is inaccurate, obtain better estimate from current time
-//           startTime = Date.now();
-//           endTime = startTime + clinic.estimatedServiceTime;
-//           // Get next timing after booked bookedSlots
-//           Object.keys(bookingList)
-//           .sort()
-//           .forEach((key) => {
-//             // The additional = in comparisons intended to resolve exact overlap
-//             // If start of queue time is between booked appointment start and end or
-//             // end of queue time is between booked appointment start and end,
-//             // Push queue to after booked appointment
-//             if ((key >= startTime && key < endTime) ||
-//               (bookingList[key] > startTime && bookingList[key] <= endTime)) {
-//               startTime = bookingList[key];
-//               endTime = bookingList[key] + clinic.estimatedServiceTime;
-//             }
-//           });
-//         }
-//
-//         // Obtain next slot (resolving for booking) after current queue
-//         let nextStartTime = endTime;
-//         let nextEndTime = endTime + clinic.estimatedServiceTime;
-//         Object.keys(bookingList)
-//         .sort()
-//         .forEach((key) => {
-//           // The additional = in comparisons intended to resolve exact overlap
-//           // If start of queue time is between booked appointment start and end or
-//           // end of queue time is between booked appointment start and end,
-//           // Push queue to after booked appointment
-//           if ((key >= nextStartTime && key < nextEndTime) ||
-//             (bookingList[key] > nextStartTime && bookingList[key] <= nextEndTime)) {
-//             nextStartTime = bookingList[key];
-//             nextEndTime = bookingList[key] + clinic.estimatedServiceTime;
-//           }
-//         });
-//         // TODO: GIVE NEGATIVE NUMBER IF MORE THAN BOOKING THRESHOLD
-//         clinic.nextEstimate = nextStartTime;
-//         appt.startTime = startTime;
-//         appt.endTime = endTime;
-//         admin.database().ref("appointmentsclinic/" + context.params.uid + "/" + context.params.entry).update(appt);
-//         return clinic;
-//       }
-//       return {};
-//     });
-//   } else {
-//     // TODO: check with realtime queue if it passes 3 hour mark
-//     let newBooking = {};
-//     return clinicRef.once("value").then((clinic) => {
-//       let clinicObj = clinic.val();
-//       appt.endTime = appt.startTime + clinicObj.estimatedServiceTime;
-//       newBooking[appt.startTime] = appt.endTime;
-//       admin.database().ref("appointmentsclinic/" + context.params.uid + "/" + context.params.entry).update(appt);
-//       return admin.database().ref("clinics/" + appt.clinic + "/bookedSlots").update(newBooking);
-//     });
-//   }
-// });
+// Add information to appointments for easy reading
+exports.addNamesToAppointments = functions.database.ref("appointments/{entry}").onCreate((snapshot, context) => {
+  const appt = snapshot.val();
+  let clinicRef = admin.database().ref("clinics/" + appt.clinic);
+  let patientRef = admin.database().ref("patients/" + appt.patient);
+  return Promise.all([
+    clinicRef.once('value'),
+    patientRef.once('value')
+  ]).then(res => {
+    appt.clinicName = res[0].val().clinicName;
+    appt.patientName = res[1].val().name;
+    return admin.database().ref("appointments/" + context.params.entry).update(appt);
+  });
+});
+
 
 // Function for joining queue, to determine the next queue time, correlate with advance booking slots with buffer
 exports.updateRealTimeEstimates = functions.database.ref("appointments/{entry}").onCreate((snapshot, context) => {
@@ -188,9 +91,17 @@ exports.updateRealTimeEstimates = functions.database.ref("appointments/{entry}")
         if (nextStartTime > Date.now() + BOOKINGTHRESHOLD) {
           nextStartTime = -nextStartTime;
         }
+
+        // tentatively put queueNum into clinic object
+        let queueNum = clinic.nextQueueNum;
+        if (queueNum === null) {
+          queueNum = 0;
+        }
+        clinic.nextQueueNum = queueNum + 1;
         clinic.nextEstimate = nextStartTime;
         appt.startTime = startTime;
         appt.endTime = endTime;
+        appt.queueNum = queueNum;
         admin.database().ref("appointments/" + context.params.entry).update(appt);
         return clinic;
       }
