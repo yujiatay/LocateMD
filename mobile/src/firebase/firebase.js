e1aa0d5071f8117990f68398d71243ea60aee74b
Yu Jia <yujiatay@gmail.com>
2018-07-24 00:12:00 +0800

    rf: Add react-native-dotenv and shift firebase config to env

--- mobile/src/firebase/firebase.js
+++ mobile/src/firebase/firebase.js
@@ -1,12 +1,16 @@
 import * as firebase from 'firebase';
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL, FIREBASE_PROJECTID,
+  FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDERID
+} from 'react-native-dotenv';
 
 const config = {
-  apiKey: 'AIzaSyAe-kdYNRZJkY90rspVXhmCNqlbhAAncqg',
-  authDomain: 'locatemd.firebaseapp.com',
-  databaseURL: 'https://locatemd.firebaseio.com',
-  projectId: 'locatemd',
-  storageBucket: 'locatemd.appspot.com',
-  messagingSenderId: '385392707364'
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECTID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDERID
 };
 
 if (!firebase.apps.length) {
